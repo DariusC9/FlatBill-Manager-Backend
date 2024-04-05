ab85e2d6f660bd292da875a5b92d0bd377551a4c
DariusC9 <coutidarius@gmail.com>
2024-04-05 09:57:49 +0300

    feat: add database for tos

---
+++ src/main/java/com/DariusC9/FlatBillManagerBackend/Database/Database.java
@@ -0,0 +1,8 @@
+package com.DariusC9.FlatBillManagerBackend.Database;
+
+public class Database {
+    private TOSData tosData;
+    public Database() {
+        tosData = new TOSData();
+    }
+}
---
+++ src/main/java/com/DariusC9/FlatBillManagerBackend/Database/TOSData.java
@@ -0,0 +1,10 @@
+package com.DariusC9.FlatBillManagerBackend.Database;
+
+public class TOSData {
+    private String data;
+    public TOSData() {
+        data = "By downloading and using this app, you agree to abide by our terms of service./n" +
+                " Enjoy a seamless experience with us!";
+    }
+    public String getData() { return data; }
+}
