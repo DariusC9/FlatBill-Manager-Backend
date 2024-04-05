7ab769cfaf2fb5e86381372d2e2a154c3dac0387
DariusC9 <coutidarius@gmail.com>
2024-04-05 09:30:20 +0300

    first commit

---
+++ src/main/java/com/DariusC9/FlatBillManagerBackend/FlatBillManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.DariusC9.FlatBillManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatBillManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatBillManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DariusC9/FlatBillManagerBackend/FlatBillManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DariusC9.FlatBillManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatBillManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
