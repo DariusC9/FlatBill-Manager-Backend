7a3dbe950f7e73d01f36716d32f17c7d722cfd96
DariusC9 <coutidarius@gmail.com>
2024-05-07 11:43:29 +0300

    refactor dtos from class to record

--- src/main/java/com/DariusC9/FlatBillManagerBackend/controller/DTO/PrivacyDTO.java
+++ src/main/java/com/DariusC9/FlatBillManagerBackend/controller/DTO/PrivacyDTO.java
@@ -1,16 +1,3 @@
 package com.DariusC9.FlatBillManagerBackend.controller.DTO;
 
-public class PrivacyDTO {
-    private String privacyText;
-    public PrivacyDTO(String privacyText) {
-        this.privacyText = privacyText;
-    }
-
-    public String getPrivacyText() {
-        return privacyText;
-    }
-
-    public void setPrivacyText(String privacyText) {
-        this.privacyText = privacyText;
-    }
-}
+public record PrivacyDTO(String privacyText) { }
--- src/main/java/com/DariusC9/FlatBillManagerBackend/controller/DTO/TosDTO.java
+++ src/main/java/com/DariusC9/FlatBillManagerBackend/controller/DTO/TosDTO.java
@@ -1,16 +1,3 @@
 package com.DariusC9.FlatBillManagerBackend.controller.DTO;
 
-public class TosDTO {
-    private String tosText;
-    public TosDTO(String tosText) {
-        this.tosText = tosText;
-    }
-
-    public String getTosText() {
-        return tosText;
-    }
-
-    public void setTosText(String tosText) {
-        this.tosText = tosText;
-    }
-}
+public record TosDTO(String tosText) { }
